b705d238cbcf64b4571c8e53223938e4a6332c50
ngquangreact <nguyen.quang24799@gmail.com>
2023-07-06 08:51:09 +0100

    add_counter_comonent_select

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
     CATEGORY = 0,
@@ -48,6 +49,9 @@ const RentModal = () => {
 
     const category = watch('category');
     const location = watch('location');
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
 
     const Map = useMemo(() => dynamic(() => import('../Map'), {
         ssr: false
@@ -130,16 +134,49 @@ const RentModal = () => {
             </div>
         )
     }
-    return <Modal 
-                title="Booking your home"
-                isOpen={rentModal.isOpen}
-                onClose={rentModal.onClose}
-                onSubmit={onNext}
-                actionLabel={actionLabel}
-                secondaryActionLable={secondaryActionLabel}
-                secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
-                body={bodyConent}
-            />
+
+    if (step === STEPS.INFO) {
+        bodyConent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Share some basics about your place"
+                    subtitle="What amenities do you have?" 
+                />
+                <Counter
+                    title="Guests"
+                    subtitle="Many of guest"
+                    value={guestCount}
+                    onChange={(value) => setCustomValue('guestCount', value)}
+                />
+                <Counter
+                    title="Rooms"
+                    subtitle="How many rooms do you have"
+                    value={roomCount}
+                    onChange={(value) => setCustomValue('roomCount', value)}
+                />
+                <Counter
+                    title="Bathrooms"
+                    subtitle="How many bathrooms do you have"
+                    value={bathroomCount}
+                    onChange={(value) => setCustomValue('bathroomCount', value)}
+                />
+            </div>
+        )
+    }
+
+    return (
+        <Modal 
+            title="Booking your home"
+            isOpen={rentModal.isOpen}
+            onClose={rentModal.onClose}
+            onSubmit={onNext}
+            actionLabel={actionLabel}
+            secondaryActionLable={secondaryActionLabel}
+            secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+            body={bodyConent}
+        />
+    )
+        
 }
 
 export default RentModal;
